a19a1d0d036a7029760afd401b0c9d29de3a2594
OGharsh <72140506+OGharsh@users.noreply.github.com>
2020-10-01 01:13:57 +0530

    Added Widgets

--- main.py
+++ main.py
@@ -37,6 +37,31 @@ class main:
     def change_bg(self):  #changing the background color canvas
         self.color_bg=colorchooser.askcolor(color=self.color_bg)[1]
         self.c['bg'] = self.color_bg
+       
+    def drawWidgets(self):
+        self.controls = Frame(self.master,padx = 5,pady = 5)
+        Label(self.controls, text='Pen Width:',font=('arial 18')).grid(row=0,column=0)
+        self.slider = ttk.Scale(self.controls,from_= 5, to = 100,command=self.changeW,orient=HORIZONTAL)
+        self.slider.set(self.penwidth)
+        self.slider.grid(row=0,column=1,ipadx=30)
+        self.controls.pack(side=LEFT)
+        
+        self.c = Canvas(self.master,width=500,height=500,bg=self.color_bg,)
+        self.c.pack(fill=BOTH,expand=True)
+
+        menu = Menu(self.master)
+        self.master.config(menu=menu)
+        filemenu = Menu(menu)
+        colormenu = Menu(menu)
+        menu.add_cascade(label='Colors',menu=colormenu)
+        colormenu.add_command(label='Brush Color',command=self.change_fg)
+        colormenu.add_command(label='Background Color',command=self.change_bg)
+        optionmenu = Menu(menu)
+        menu.add_cascade(label='Options',menu=optionmenu)
+        optionmenu.add_command(label='Clear Canvas',command=self.clear)
+        optionmenu.add_command(label='Exit',command=self.master.destroy) 
+        
+        
 
    
 
